77ae7a6acf0ad6e58f20d76fdf677c3a27254325
2194063 <jeromegarcia.16@gmail.com>
2023-11-06 09:39:31 +0800

    [FAILED]: Check the input ISBN String if equal 10 digits

--- UserService/src/main/java/com/csvmasterstdd/service/IsbnCleanService.java
+++ UserService/src/main/java/com/csvmasterstdd/service/IsbnCleanService.java
@@ -2,10 +2,13 @@ package com.csvmasterstdd.service;
 
 public class IsbnCleanService {
 
-
-
     public String isbnClean(Isbn isbn){
         String cleanedIsbn = isbn.getIsbnNumber().replaceAll("[\\s-]", "");
         return cleanedIsbn;
+    }
+
+    public boolean isEqualTen(Isbn isbn) {
+
+        return true;
     }
 }
--- UserService/src/test/java/IsbnTest.java
+++ UserService/src/test/java/IsbnTest.java
@@ -9,14 +9,14 @@ public class IsbnTest {
 
     @DisplayName("Hyphen are removed in Input ISBN String")
     @Test
-    void isbnCleanService_WhenInputIsbn10_shouldRemovedAllHyphen(){
+    void isbnClean_WhenInputIsbn10_shouldRemovedAllHyphen(){
         //Arrange
         IsbnCleanService isbnCleanService = new IsbnCleanService();
-        Isbn isbnWithHyphen =  Isbn.of("0-9320-2342-4");
+        Isbn isbn =  Isbn.of("0-9320-2342-4");
         String isbnExpected = "0932023424";
 
         //Act
-        String isbnCleanedActual = isbnCleanService.isbnClean(isbnWithHyphen);
+        String isbnCleanedActual = isbnCleanService.isbnClean(isbn);
 
         //Assert
         assertEquals(isbnExpected,isbnCleanedActual,"Reformatting of Input ISBN is not proper.!");
@@ -24,16 +24,30 @@ public class IsbnTest {
 
     @DisplayName("Spaces are removed in Input ISBN String")
     @Test
-    void isbnCleanService_WhenInputIsbn10_shouldRemovedAllSpaces(){
+    void isbnClean_WhenInputIsbn10_shouldRemovedAllSpaces(){
         //Arrange
         IsbnCleanService isbnCleanService = new IsbnCleanService();
-        Isbn isbnWithSpace =  Isbn.of("0-9320-2342-4");
+        Isbn isbn =  Isbn.of("0-9320-2342-4");
         String isbnExpected = "0932023424";
 
         //Act
-        String isbnCleanedActual = isbnCleanService.isbnClean(isbnWithSpace);
+        String isbnCleanedActual = isbnCleanService.isbnClean(isbn);
 
         //Assert
         assertEquals(isbnExpected,isbnCleanedActual,"Reformatting of Input ISBN is not proper.!");
+    }
+
+    @DisplayName("The Input ISBN-10 Not Equal 10 digits")
+    @Test
+    void isbnClean_WhenIsbn10NotEqualTen_shouldReturnFalse(){
+        //Arrange
+        IsbnCleanService isbnCleanService = new IsbnCleanService();
+        Isbn isbn =  Isbn.of("0-9320-2342-4");
+        boolean expected = false;
+        //Act
+        boolean actual = isbnCleanService.isEqualTen(isbn);
+
+        //Assert
+        assertEquals(expected, actual, "Result is expected false");
     }
 }
